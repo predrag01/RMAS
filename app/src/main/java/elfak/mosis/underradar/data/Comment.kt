a31d8d27a2fa243031792705c02a6e91fc332123
Predrag <predrag.tosic@elfak.rs>
2023-07-03 19:35:32 +0200

    Deleted everything

--- app/src/main/java/elfak/mosis/underradar/data/Comment.kt
+++
@@ -1,9 +0,0 @@
-package elfak.mosis.underradar.data
-
-import java.util.UUID
-
-data class Comment(
-    var id: String= UUID.randomUUID().toString(),
-    var comment: String="",
-    var CommenterId: String=""
-)
